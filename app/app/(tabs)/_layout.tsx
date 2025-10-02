b7f8223f9b5d74e9b053b2a07c43a23ece4e193b
Yuuta <yuutaman0424ku@gmail.com>
2025-10-03 02:22:41 +0900

    2025/10/03 手紙画面

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import { Tabs } from "expo-router";
 export default function TabsLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ title: "ホーム" }} />
-      <Tabs.Screen name="settings" options={{ title: "設定" }} />
-      <Tabs.Screen name="profile" options={{ title: "プロフィール" }} />
+      <Tabs.Screen name="home" options={{ title: "ホーム" , headerShown: false }} />
+      <Tabs.Screen name="letters" options={{ title: "お手紙" , headerShown: false}} />
+      <Tabs.Screen name="profile" options={{ title: "プロフィール", headerShown: false}} />
     </Tabs>
   );
 }
